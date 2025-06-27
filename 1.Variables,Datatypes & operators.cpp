576e009f843bf304612b717abc337662908ab03f
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-06-27 11:19:14 +0530

    Update 1.Variables,Datatypes & operators.cpp

--- 1.Variables,Datatypes & operators.cpp
+++ 1.Variables,Datatypes & operators.cpp
@@ -38,3 +38,40 @@ cout<<sizeof(bool)<<"\n";       //output:1
 
 -----------------------------
 
+// Type Casting : Converting data from one type to another
+// 1.Conversion - implicit, from small datatype to big datatype
+#include<iostream>
+using namespace std;
+int main(){
+char grade='A';
+int value=grade;
+cout<<value;    //output: 65(ASCII VALUE)
+return 0;
+}
+
+-----
+
+//2.Casting - explicit, from big datatype to small datatype
+#include<iostream>
+using namespace std;
+int main(){
+double price=100.99;
+int value=(int)price;
+cout<<value;    //output: 100
+return 0;
+}
+
+-------------------------------
+
+//USER INPUT
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    cout<<"Your age is "<<age;
+    return 0;
+}
+
+-------------------------------
+
