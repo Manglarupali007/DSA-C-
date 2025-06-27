d86911bac7ee2a10489d6587b4736ef270cab50a
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-06-27 11:04:34 +0530

    Update 1.Variables,Datatypes & operators.cpp

--- 1.Variables,Datatypes & operators.cpp
+++ 1.Variables,Datatypes & operators.cpp
@@ -1,8 +1,40 @@
 //Boilerplate Code
 #include<iostream>
 using namespace std;
-
 int main(){
-    
     return 0;
 }
+
+---------------------------
+    
+//Variables : containers to store data
+#include<iostream>
+using namespace std;
+int main(){
+int age=20;    
+float marks=93.5;   
+char grade='A';
+string name="Aman";
+cout<<age<<"\n";
+cout<<marks<<"\n";
+cout<<grade<<"\n";
+cout<<name<<"\n";
+return 0;
+}
+
+------------------------------
+
+//size of datatypes - (Primitive Datatypes)
+#include<iostream>
+using namespace std;
+int main(){
+cout<<sizeof(int)<<"\n";        //output:4
+cout<<sizeof(char)<<"\n";       //output:1
+cout<<sizeof(float)<<"\n";      //output:4
+cout<<sizeof(double)<<"\n";     //output:8
+cout<<sizeof(bool)<<"\n";       //output:1
+    return 0;
+}
+
+-----------------------------
+
